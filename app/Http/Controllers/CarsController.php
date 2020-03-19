93a602387d8af725dba9fdbbb1b17f104b8d29cc
Andre luis <andre.rodrigues98@outlook.com>
2020-03-19 10:57:42 -0300

    inserindo melhorias de códigos

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Cars;
 
 class CarsController extends Controller
@@ -12,35 +14,88 @@ class CarsController extends Controller
      *
      * @return void
      */
+
     public function __construct(Cars $cars)
     {
       $this->model = $cars;   
     }
 
     public function getAll(Request $request){
-      $cars = $this->model->all();
-      return response()->json($cars);
+      try{
+        $cars = $this->model->all();
+        if(count($cars) > 0)
+          return response()->json($cars, Response::HTTP_OK);
+        else
+          return response()->json([], Response::HTTP_OK);
+      }catch (QueryException $exception){
+        return response()->json(["error" => "There's not database connection!"], Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
     }
 
     public function get($id, Request $request){
-      $car = $this->model->find($id);
-      return response()->json($car);
+      try {
+        $car = $this->model->find($id);
+        if(count($car) > 0)
+          return response()->json($car, Response::HTTP_OK);
+        else
+          return response()->json(null, Response::HTTP_OK);
+      }catch (QueryException $exception){
+        return response()->json(["error" => "There's not database connection!"], Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
     }
 
     public function store(Request $request){
-      $car = $this->model->create($request->all());
-      return response()->json($car);
+      try{
+        $validator = Validator::make(
+          $request->all(),
+          [
+            'name' => 'required | max:80',
+            'description' => 'required',
+            'model' => 'required | max:10 | min:2',
+          ]
+        );
+        
+        if($validator -> fails()){
+          return response()->json([$validator -> errors()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $car = $this->model->create($request->all());
+        return response()->json($car, Response::HTTP_CREATED);
+      }catch (QueryException $exception){
+        return response()->json(["error" => "There's not database connection!"], Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
     }
 
     public function update($id, Request $request){
-      $car = $this->model->find($id)
-        ->update($request->all());
-      return response()->json($car);
+      try{
+        $validator = Validator::make(
+          $request->all(),
+          [
+            'name' => 'required | max:80',
+            'description' => 'required',
+            'model' => 'required | max:10 | min:2',
+          ]
+        );
+        
+        if($validator -> fails()){
+          return response()->json([$validator -> errors()], Response::HTTP_BAD_REQUEST);
+        }
+        
+        $car = $this->model->find($id)
+          ->update($request->all());
+          return response()->json($car, Response::HTTP_OK);
+      }catch (QueryException $exception){
+        return response()->json(["error" => "There's not database connection!"], Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
     }
 
     public function destroy($id, Request $request){
-      $car = $this->model->find($id)
-        ->delete();
-      return response()->json(null);
+      try{
+        $car = $this->model->find($id)
+          ->delete();
+        return response()->json(null, Response::HTTP_OK);
+      }catch (QueryException $exception){
+        return response()->json(["error" => "There's not database connection!"], Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
     }
 }
